559d2db05a786c4b908933d7c34ff17bdd0f0b48
Efthymia.Kazakou <Efthymia.Kazakou@huddle.com>
2017-11-07 12:00:07 +0000

    Added first 6 exercises

---
+++ ex5.py
@@ -0,0 +1,21 @@
+my_name = 'Efthymia Kazakou'
+my_age = 27
+my_height = 173
+my_weight = 65
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Blond'
+
+print(f"Let's talk about {my_name}")
+print(f"She's {my_height} cm tall")
+print(f"She's {my_weight} kg heavy")
+
+print(f"She's got {my_eyes} eyes and {my_hair} hair") 
+print(f"Her teeth are usually {my_teeth} depending on the coffee")
+
+total = my_age + my_height + my_weight
+print (f"If I add {my_age}, {my_height} and {my_weight} I get {total}.") 
+
+ 
+ 
+ 
