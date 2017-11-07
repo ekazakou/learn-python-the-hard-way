2aa9870c06ea5c66635ba499e1deef936b4c0fb2
Efthymia.Kazakou <Efthymia.Kazakou@huddle.com>
2017-11-07 17:29:40 +0000

    Added exercises 6-11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weigh?")
+weight = input()
+
+print(f"So you are {age} years old, {height} tall and {weight} heavy")
